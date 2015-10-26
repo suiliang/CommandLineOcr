0a35a9b964c690e25690dd14d5ac06a3b6494f97
Sui Liang <sui.liang@hotmail.com>
2015-10-26 16:47:40 +0100

    Use json object as output data

--- CommandLineOcr/CommandLineOcr.Shared/App.xaml.cs
+++ CommandLineOcr/CommandLineOcr.Shared/App.xaml.cs
@@ -1,26 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 using WindowsPreview.Media.Ocr;
-using System.IO;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Windows.Data.Json;
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
 namespace CommandLineOcr
@@ -46,12 +39,12 @@ namespace CommandLineOcr
 
         protected async override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
             if (!string.IsNullOrEmpty(e.Arguments))
             {
                 try
                 {
-                    await WriteToFile(folder,"arg.txt",e.Arguments);
+                    await WriteToFile(folder, "arg.txt", e.Arguments);
                     var ocrEngine = new OcrEngine(OcrLanguage.English);
                     var file = await folder.GetFileAsync(e.Arguments);
                     ImageProperties imgProp = await file.Properties.GetImagePropertiesAsync();
@@ -79,34 +72,43 @@ namespace CommandLineOcr
                     // OCR result does not contain any lines, no text was recognized. 
                     if (ocrResult.Lines != null)
                     {
-                        string extractedText = "";
+                        JsonObject jsonOjbect = new JsonObject();
+                        jsonOjbect.Add("text_angle", JsonValue.CreateNumberValue(ocrResult.TextAngle.HasValue ? ocrResult.TextAngle.Value : 0d));
+
+                        JsonArray wordsArray = new JsonArray();
+                        jsonOjbect.Add("words", wordsArray);
 
                         // Iterate over recognized lines of text.
                         foreach (var line in ocrResult.Lines)
                         {
-                            // Iterate over words in line.
                             foreach (var word in line.Words)
                             {
-                                extractedText += word.Text + " ";
+                                JsonObject wordJson = new JsonObject();
+                                wordsArray.Add(wordJson);
+                                wordJson.Add("top", JsonValue.CreateNumberValue(word.Top));
+                                wordJson.Add("left", JsonValue.CreateNumberValue(word.Left));
+                                wordJson.Add("width", JsonValue.CreateNumberValue(word.Width));
+                                wordJson.Add("height", JsonValue.CreateNumberValue(word.Height));
+                                wordJson.Add("text", JsonValue.CreateStringValue(word.Text));
                             }
-                            extractedText += Environment.NewLine;
                         }
-                        await WriteToFile(folder, file.Name + ".txt", extractedText);
+                        await WriteToFile(folder, file.Name + ".txt", jsonOjbect.Stringify());
                     }
                     else
                     {
 
-                        WriteToFile(folder, "failed.txt", "No Text");
+                        await WriteToFile(folder, "failed.txt", "No Text");
                     }
 
                 }
                 catch (Exception ex)
                 {
-                    WriteToFile(folder, "failed.txt", ex.Message + "\r\n"+ex.StackTrace);
+                    await WriteToFile(folder, "failed.txt", ex.Message + "\r\n"+ex.StackTrace);
                 }
                 App.Current.Exit();
             }
         }
+
         private async Task WriteToFile(StorageFolder folder, string fileName,string extractedText)
         {
             // Get the text data from the textbox. 
